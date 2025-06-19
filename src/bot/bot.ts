c1a10b8874664aa0ace7899b14b25566a038eed9
U82146510 <U82146510@protonmail.com>
2025-06-19 12:12:08 +0100

    working

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,7 +1,8 @@
-import { InlineKeyboard,Bot,Context } from "grammy";
+import { InlineKeyboard,Bot,Context, Api } from "grammy";
 import dotenv from "dotenv";
 import {fileURLToPath} from 'url';
 import path from "path";
+import {logger} from './logger/logger.ts';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,3 +10,11 @@ const __dirname = path.dirname(__filename);
 dotenv.config({
     path:path.resolve(__dirname,'../../.env')
 });
+
+const token = process.env.bot_token;
+if(!token){
+    throw new Error('missing telegram token');
+}
+
+const bot:Bot<Context,Api> = new Bot(token);
+
