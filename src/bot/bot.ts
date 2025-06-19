75c8209be00ce18db6d095cb014b56655bf21436
U82146510 <U82146510@protonmail.com>
2025-06-19 12:02:52 +0100

    first commitment

---
+++ src/bot/bot.ts
@@ -0,0 +1,11 @@
+import { InlineKeyboard,Bot,Context } from "grammy";
+import dotenv from "dotenv";
+import {fileURLToPath} from 'url';
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+    path:path.resolve(__dirname,'../../.env')
+});
